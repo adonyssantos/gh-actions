510e61e09ebf79ae1d1e75712608053dcb981488
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-21 14:28:30 -0400

    Fix another bug

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: [
-    'https://adonyssantos.github.io/gh-actions/',
-    'https://gh-actions-3256c.firebaseapp.com/',
-    'https://gh-actions-3256c.web.app/',
-  ],
+  base: 'https://gh-actions-3256c.web.app/',
   plugins: [react()],
 });
