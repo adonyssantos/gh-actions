afeb34b6db75923f064764b1f58f170d47d63147
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-21 11:50:51 -0400

    Update base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://adonyssantos.github.io/',
+  base: 'https://adonyssantos.github.io/gh-actions/',
   plugins: [react()],
 });
